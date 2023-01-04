b7ebba916cd1cb4d2a6b6fef7d11f848779d271f
Jayesh Saini <jayesh.saini.95@gmail.com>
2023-01-03 21:34:40 -0500

    fixed React Image issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/legacy/image";
 import {
     MenuIcon,
     SearchIcon,
@@ -21,7 +21,7 @@ function Header() {
                 </div>
 
                 {/* Search */}
-                <div className="hidden sm:flex ml-6 items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+                <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
                     <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
                     <SearchIcon className='h-12 p-4' />
                 </div>
