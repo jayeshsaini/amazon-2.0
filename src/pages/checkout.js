7011f169ad655f4f9c9020db6eb4857d776f6cd3
Jayesh Saini <jayesh.saini.95@gmail.com>
2023-01-05 00:52:38 -0500

    implement stripe webhooks

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,12 +5,33 @@ import { selectItems, selectTotal } from "../slices/basketSlice"
 import CheckoutProduct from "../components/CheckoutProduct"
 import { useSession } from "next-auth/react"
 import Currency from "react-currency-formatter"
+import axios from "axios"
+import { loadStripe } from "@stripe/stripe-js";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function checkout() {
 
     const items = useSelector(selectItems);
     const total = useSelector(selectTotal);
     const { data: session } = useSession();
+
+    const createCheckoutSession = async () => {
+        const stripe = await stripePromise;
+
+        // Call the backend to create a checkout session...
+        const checkoutSession = await axios.post('/api/create-checkout-session',
+            {
+                items: items,
+                email: session.user.email
+            })
+
+        // Redirect user/customer to Stripe Checkout
+        const result = await stripe.redirectToCheckout({
+            sessionId: checkoutSession.data.id
+        });
+
+        if (result.error) alert(result.error.message);
+    };
 
     return (
         <div className="bg-gray-100">
@@ -59,6 +80,8 @@ function checkout() {
                             </h2>
 
                             <button
+                                role="link"
+                                onClick={createCheckoutSession}
                                 disabled={!session}
                                 className={`button mt-2 ${!session && "from-gray-300 to-gray-500 border-gray-200 text-grey-300 cursor-not-allowed"
                                     }`}
