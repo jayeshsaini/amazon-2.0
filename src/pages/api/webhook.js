a58a9add4fdd3faebb3a34e6d7bb822b2dfdfb3d
Jayesh Saini <jayesh.saini.95@gmail.com>
2023-01-06 21:07:38 -0500

    fixed service account firebase

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -2,7 +2,9 @@ import { buffer } from 'micro';
 import * as admin from 'firebase-admin';
 
 // Secure a connection to Firebase from the backend
-const serviceAccount = require('../../../permissions.json');
+const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+);
 
 const app = !admin.apps.length ? admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
